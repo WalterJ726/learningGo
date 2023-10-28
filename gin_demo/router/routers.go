eed1ae06f1c7b684ec8d1051b9bd00f6894210e9
WalterJ726 <walterjiang726@gmail.com>
2023-10-28 21:53:26 +0800

    添加routers路由

--- gin_demo/router/routers.go
+++ gin_demo/router/routers.go
@@ -3,26 +3,44 @@ package router
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"gin_demo/controllers"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
 
-	user := r.Group("/user") // 注册路由
+	// user := r.Group("/user") // 注册路由
+	// {
+	// 	user.POST("/register", func(ctx *gin.Context) {
+	// 		ctx.String(http.StatusOK, "user register")
+	// 	})
+	// 	user.POST("/login", func(ctx *gin.Context) {
+	// 		ctx.String(http.StatusOK, "user login")
+	// 	})
+	// }
+	// player := r.Group("/player")
+	// {
+	// 	player.POST("/list", func(ctx *gin.Context) {
+	// 		ctx.String(http.StatusOK, "player list")
+	// 	})
+	// }
+
+	// return r
+
+	user := r.Group("/user")
 	{
-		user.POST("/register", func(ctx *gin.Context) {
-			ctx.String(http.StatusOK, "user register")
-		})
-		user.POST("/login", func(ctx *gin.Context) {
-			ctx.String(http.StatusOK, "user login")
-		})
+		user.POST("/register", controllers.UserController{}.Register)
+		user.POST("/login", controllers.UserController{}.Login)
 	}
 	player := r.Group("/player")
 	{
-		player.POST("/list", func(ctx *gin.Context) {
-			ctx.String(http.StatusOK, "player list")
-		})
+		player.POST("/list", controllers.PlayerController{}.GetPlayers)
 	}
+	vote := r.Group("/vote")
+	{
+		vote.POST("/add", controllers.VoteController{}.AddVote)
+	}
+	r.POST("/ranking", controllers.PlayerController{}.GetRanking)
 
-	return r
+	return r	
 }
